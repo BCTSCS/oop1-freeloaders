d987281d9be695972cafe3e3333c4a0709c563b5
Grace <grajun26@bergen.org>
2025-10-27 14:10:36 -0400

    10/27

---
+++ src/main/java/Player.java
@@ -0,0 +1,19 @@
+public class Player {
+
+    private String name;
+    private int level;
+
+    public Player() {
+        this.name = "Grace";
+        this.level = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+    
+}
